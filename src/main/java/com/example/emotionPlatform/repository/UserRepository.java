6320758553f5eae3abad2842d632ce660940f497
Andrea <foriglio.andrea@gmail.com>
2026-07-26 14:02:28 +0200

    Rimosso import List da userRepository

--- src/main/java/com/example/emotionPlatform/repository/UserRepository.java
+++ src/main/java/com/example/emotionPlatform/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.emotionPlatform.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.emotionPlatform.entity.User;
-import java.util.List;
 import java.util.Optional;
 
 
