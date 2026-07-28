0587e05e1a57b153e305edd8ece6f2e26a6a5794
Andrea <foriglio.andrea@gmail.com>
2026-07-28 19:40:03 +0200

    Implementazione di StringUtilsCustom in UserMapper

--- src/main/java/com/example/emotionPlatform/mapper/UserMapper.java
+++ src/main/java/com/example/emotionPlatform/mapper/UserMapper.java
@@ -1,13 +1,30 @@
 package com.example.emotionPlatform.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.emotionPlatform.dto.user.UserRequestDTO;
 import com.example.emotionPlatform.dto.user.UserResponseDTO;
 import com.example.emotionPlatform.entity.User;
+import com.example.emotionPlatform.utils.StringUtilsCustom;
 
-@Mapper(componentModel = "spring", uses = {RoleMapper.class, DepartmentMapper.class} )
+@Mapper(
+    componentModel = "spring", 
+    uses = {RoleMapper.class, DepartmentMapper.class}, 
+    imports = StringUtilsCustom.class )
+
 public interface UserMapper {
+
+    // expression esegue codice java personalizzato durante il mapping
+    @Mapping(
+        target = "firstName",
+        expression = "java(StringUtilsCustom.capitalizeWords(dto.getFirstName()))"
+    )
+    @Mapping(
+        target = "lastName",
+        expression = "java(StringUtilsCustom.capitalizeWords(dto.getLastName()))"
+    )
+    
     User toEntity(UserRequestDTO dto);
     
     UserResponseDTO toResponse(User user);
