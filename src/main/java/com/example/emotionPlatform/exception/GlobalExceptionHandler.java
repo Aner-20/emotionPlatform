2f1c499ca7215fe6f579389f756a49be4ed54acc
Andrea <foriglio.andrea@gmail.com>
2026-07-29 13:37:56 +0200

    Commenti a GlobalExceptionHandler.java

--- src/main/java/com/example/emotionPlatform/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/emotionPlatform/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.example.emotionPlatform.dto.error.ErrorResponse;
 
 // @RestControllerAdvice: intercetta tutte le eccezioni in tutta l'applicazione
+// Utile in modo che non si debba scrive sempre try/catch
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
@@ -32,23 +33,26 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(UnAuthorizedException.class)
+    // ResponseEntity: oggetto Spring che rappresenta una risposta HTTP completa
     public ResponseEntity<ErrorResponse> handleUnauthorized(
             UnAuthorizedException exception
     ){
 
+        // Crea un oggetto DTO che rapprensenta l'errore
         ErrorResponse error = new ErrorResponse(
                 LocalDateTime.now(),
                 401,
                 exception.getMessage()
         );
 
-
+        // ResponseEntity: crea la risposta HTTP
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(error);
     }
 
 
+    // @ExceptionHandler: serve a dire quale metodo deve essere eseguito quando viene lanciata una certa eccezione
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ErrorResponse> handleValidation(
             ValidationException exception
