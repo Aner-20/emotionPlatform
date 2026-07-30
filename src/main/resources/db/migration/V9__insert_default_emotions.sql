04258d226af81f37e697dfed907294264fbdae1a
Andrea <foriglio.andrea@gmail.com>
2026-07-30 16:58:46 +0200

    Aggiunti V8 e V9 alla cartella db

---
+++ src/main/resources/db/migration/V9__insert_default_emotions.sql
@@ -0,0 +1,9 @@
+INSERT INTO emotions (name, color)
+VALUES
+    ('JOY', '#FFD700'),
+    ('SADNESS', '#0000FF'),
+    ('ANGER', '#FF0000'),
+    ('FEAR', '#800080'),
+    ('SURPRISE', '#FFA500'),
+    ('DISGUST', '#008000'),
+    ('NEUTRAL', '#808080');
