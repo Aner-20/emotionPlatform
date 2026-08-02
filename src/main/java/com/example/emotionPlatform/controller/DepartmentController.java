01274ab05e0fd1d4e2727d3af733ce7728fe0989
Andrea <foriglio.andrea@gmail.com>
2026-08-02 15:33:44 +0200

    Aggiornato DepartmentMapper e creato DepartmentController

--- src/main/java/com/example/emotionPlatform/controller/DepartmentController.java
+++ src/main/java/com/example/emotionPlatform/controller/DepartmentController.java
@@ -1,5 +1,66 @@
 package com.example.emotionPlatform.controller;
 
+import org.springframework.http.ResponseEntity;
+
+import com.example.emotionPlatform.dto.department.DepartmentRequestDTO;
+import com.example.emotionPlatform.dto.department.DepartmentResponseDTO;
+import com.example.emotionPlatform.service.DepartmentService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
+import java.util.*;
+
+@RestController
+@RequestMapping("api/departments")
+@RequiredArgsConstructor
+
 public class DepartmentController {
     
+    private final DepartmentService departmentService;
+
+    @PostMapping
+    public ResponseEntity<DepartmentResponseDTO> createDeparment(@Valid @RequestBody DepartmentRequestDTO request) {
+
+        DepartmentResponseDTO response = departmentService.createDepartment(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentResponseDTO>> getAllDepartments() {
+
+        return ResponseEntity.ok(departmentService.getAllDepartments());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DepartmentResponseDTO> getDepartmentById(@PathVariable Long id){
+        // ok() crea una risposta HTTP con status 200 ok
+        return ResponseEntity.ok(departmentService.getDepartmentById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DepartmentResponseDTO> updateDepartment(
+            @PathVariable Long id,
+            @Valid @RequestBody DepartmentRequestDTO request
+    ) {
+
+        return ResponseEntity.ok(departmentService.updateDepartment(id, request));
+    }
+
+     @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDepartment(@PathVariable Long id) {
+
+        departmentService.deleteDepartment(id);
+
+        return ResponseEntity
+                .noContent() // crea risposta con status 204 NO CONTENT
+                .build(); // costruisce l'oggetto in modo definitivo
+    }
+
 }
--- src/main/java/com/example/emotionPlatform/mapper/DepartmentMapper.java
+++ src/main/java/com/example/emotionPlatform/mapper/DepartmentMapper.java
@@ -1,6 +1,7 @@
 package com.example.emotionPlatform.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.emotionPlatform.dto.department.DepartmentRequestDTO;
 import com.example.emotionPlatform.dto.department.DepartmentResponseDTO;
@@ -8,6 +9,12 @@ import com.example.emotionPlatform.entity.Department;
 
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
+    
+    // ignore ignora i campi dell'entity Department
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "users", ignore = true)
     Department toEntity(DepartmentRequestDTO dto);
+    
+    
     DepartmentResponseDTO toResponse(Department department);
 }
