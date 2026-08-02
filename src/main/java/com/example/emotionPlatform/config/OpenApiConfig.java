de076e55e861be5c138d674405a8592e2313e8af
Andrea <foriglio.andrea@gmail.com>
2026-08-02 15:10:12 +0200

    Security e OpenApi config implementati

--- src/main/java/com/example/emotionPlatform/config/OpenApiConfig.java
+++ src/main/java/com/example/emotionPlatform/config/OpenApiConfig.java
@@ -1,6 +1,23 @@
 package com.example.emotionPlatform.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+
 // Per personalizzare Swagger
+@Configuration
 public class OpenApiConfig {
     
+    @Bean
+    public OpenAPI emotionPlatformOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Emotion Platform API")
+                        .description("REST API for notes handling and emotions analysis through AI.")
+                        .version("1.0.0")
+
+                    
+                    );
+                }
 }
---
+++ src/main/java/com/example/emotionPlatform/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.emotionPlatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // dice a spring che questa classe contiene configurazioni da caricare all'avvio dell'applicazione
+public class SecurityConfig {
+
+    // HttpSecurity http: oggetto con cui si configura Spring security
+    // Csrf: Cross-Site Request Forgery, protezione contro richieste malevole provenienti da un altro sito
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+
+            // Disabilita CSRF per le API REST
+            .csrf(csrf -> csrf.disable())
+
+            // Autorizzazioni
+            .authorizeHttpRequests(auth -> auth
+
+                // Swagger
+                .requestMatchers(
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**"
+                ).permitAll()
+
+                // Tutte le API (solo durante lo sviluppo)
+                .anyRequest().permitAll()
+            )
+
+            // Login HTTP Basic (non verrà usato perché tutto è permitAll)
+            // Il browser mostra una finestra con Username: e password: 
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
