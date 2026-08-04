c31a444127b9db6111689ef2ae31ae74fc6a6de1
Andrea <foriglio.andrea@gmail.com>
2026-08-04 14:55:32 +0200

    Implementato Spring security in User

--- src/main/java/com/example/emotionPlatform/entity/User.java
+++ src/main/java/com/example/emotionPlatform/entity/User.java
@@ -4,7 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails; // Spring security
 
 @Entity
 @Table(name = "users")
@@ -14,7 +19,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -52,6 +57,41 @@ public class User {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    // Restituisce i permessi/ruoli dell'utente
+    // Spring security non conosce la classe Role, quindi ha bisogno di GrantedAuthroity
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // role.getName().name() per via di RoleType
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.getName().name()));
+    }
+
+
+    // In realtà dovrebbe essere getEmail
+    @Override
+    public String getUsername() {
+        return email; 
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     
 }
