fdb37c8c324f34b881cf9e7ecd07c60843259a8e
Andrea <foriglio.andrea@gmail.com>
2026-08-19 16:33:01 +0200

    Creato AiAnalysisRequestDTO

---
+++ src/main/java/com/example/emotionPlatform/dto/ai/AiAnalysisRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.emotionPlatform.dto.ai;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AiAnalysisRequestDTO {
+    private String text;
+}
