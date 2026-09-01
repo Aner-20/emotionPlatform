103ef284345f61d760c2d785ef802ff9f6bb2bdc
Andrea <foriglio.andrea@gmail.com>
2026-09-01 19:09:19 +0200

    Modal di registrazione implementato

--- frontend/src/components/card/RegisterCard.jsx
+++ frontend/src/components/card/RegisterCard.jsx
@@ -15,6 +15,10 @@ function RegisterCard(){
     const [email, setEmail] = useState("");
     const [departmentId, setDepartmentId] = useState("");
 
+    const [showModal, setShowModal] = useState(false);
+    
+
+
     const isPasswordValid =  password.length >= 8 &&
     /[A-Z]/.test(password) &&
     /[a-z]/.test(password) &&
@@ -56,7 +60,13 @@ function RegisterCard(){
         if (isFirstNameValid && isLastNameValid && isPasswordValid && isValidEmail(email)){
             try {
                 await register(firstName, lastName, email, password, departmentId);
-                console.log(departmentId);
+                setShowModal(true);
+                setFirstName("");
+                setLastName("");
+                setPassword("");
+                setEmail("");
+                setDepartmentId("");
+
             } catch (error) {
                 console.error("Errore durante la registrazione:", error);
             }
@@ -70,48 +80,67 @@ function RegisterCard(){
     // value={department.id} il valore associato a questo elemento HTML deve essere department.id
     // && se la condizione è vera mostra l'elemento. condizione && elemento
     return (
-        <div className="register-card">
-            <div className="data-user-section">
-                <input type="text" placeholder="Nome" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                {firstName.length >= 0 && (
-                    <p>
-                        {isFirstNameValid ? "✓ Nome valido" :  "✗ Il nome deve avere almeno 3 caratteri"}
-                    </p>
-                )}
-                <input type="text" placeholder="Cognome" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                {lastName.length >= 0 && (
-                    <p>
-                        {isLastNameValid ? "✓ Cognome valido" :  "✗ Il cognome deve avere almeno 3 caratteri"}
-                    </p>
-                )}
-                <div className="password-field">
-                        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                        {password.length >= 0 && (
-                            <div className="password-requirements">
-                                <p>{password.length >= 8 ? "✓" : "✗"} Almeno 8 caratteri</p>
-                                <p>{/[A-Z]/.test(password) ? "✓" : "✗"} Una lettera maiuscola</p>
-                                <p>{/[a-z]/.test(password) ? "✓" : "✗"} Una lettera minuscola</p>
-                                <p>{/[0-9]/.test(password) ? "✓" : "✗"} Un numero</p>
-                            </div>
-                        )}
+        <>
+            <div className="register-card">
+                <div className="data-user-section">
+                    <input type="text" placeholder="Nome" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                    {firstName.length >= 0 && (
+                        <p>
+                            {isFirstNameValid ? "✓ Nome valido" :  "✗ Il nome deve avere almeno 3 caratteri"}
+                        </p>
+                    )}
+                    <input type="text" placeholder="Cognome" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    {lastName.length >= 0 && (
+                        <p>
+                            {isLastNameValid ? "✓ Cognome valido" :  "✗ Il cognome deve avere almeno 3 caratteri"}
+                        </p>
+                    )}
+                    <div className="password-field">
+                            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            {password.length >= 0 && (
+                                <div className="password-requirements">
+                                    <p>{password.length >= 8 ? "✓" : "✗"} Almeno 8 caratteri</p>
+                                    <p>{/[A-Z]/.test(password) ? "✓" : "✗"} Una lettera maiuscola</p>
+                                    <p>{/[a-z]/.test(password) ? "✓" : "✗"} Una lettera minuscola</p>
+                                    <p>{/[0-9]/.test(password) ? "✓" : "✗"} Un numero</p>
+                                </div>
+                            )}
+                    </div>
+                    
+                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    {email.length >= 0 && (
+                        isValidEmail(email) ? <p>Email valida</p> : <p>Email non valida</p>
+                    )}
+                    <select value={departmentId} onChange={(e) => setDepartmentId(Number(e.target.value))}>
+                        {departments.map((department) => (
+                            <option key={department.id} value={department.id}>{department.name}</option>
+                        ))}
+                    </select>
+                    
+                    
                 </div>
-                
-                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                {email.length >= 0 && (
-                    isValidEmail(email) ? <p>Email valida</p> : <p>Email non valida</p>
-                )}
-                <select value={departmentId} onChange={(e) => setDepartmentId(Number(e.target.value))}>
-                    {departments.map((department) => (
-                        <option key={department.id} value={department.id}>{department.name}</option>
-                    ))}
-                </select>
-                
-                
+                <div className="button-section">
+                    <button className="registration-button" onClick={registerUser} 
+                    disabled={
+                         !isFirstNameValid ||
+                         !isLastNameValid ||
+                         !isPasswordValid ||
+                         !isValidEmail(email)
+                    }>Registrati</button>
+                </div>
             </div>
-            <div className="button-section">
-                <button onClick={registerUser}>Registrati</button>
-            </div>
-        </div>
+        
+        {showModal && (
+            
+            <Modal
+                    title="Modulo di registrazione"
+                    message="Registrazione effettuata con successo!"
+                    onClose={() => setShowModal(false)}
+                />  
+                
+            )}
+
+       </> 
     )
 }
 
